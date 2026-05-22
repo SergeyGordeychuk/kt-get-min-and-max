4d63359efab9189b1c89c59038a6ec213b34e890
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2026-05-22 18:40:23 +0300

    Created work

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
-
+    require(inputArray.isNotEmpty()) { "Array must not be empty" }
+    val min = inputArray.minOrNull()!!
+    val max = inputArray.maxOrNull()!!
+    return intArrayOf(min, max)
 }
